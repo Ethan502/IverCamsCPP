7da8db23942e8c63dbeca2472745315e870ca267
Ethan <esmit502@byu.edu>
2022-08-17 16:51:43 -0600

    first commit for the single cam file

--- VimbaCPP/Examples/myMulti/Console/Source/main.cpp
+++ VimbaCPP/Examples/myMulti/Console/Source/main.cpp
@@ -13,163 +13,57 @@
 #define CAMERA_COUNT 3
 
 
-//void grabber(AVT::VmbAPI::Examples::ApiController &control,
-//               AVT::VmbAPI::Examples::ProgramConfig &fig);
+std::string cmdParse(int argc, char* argv[]);
+void printHelp(char* argv[]);
 
-int main()
+int main(int argc, char* argv[])
 {
     VmbErrorType err; //initialize the variable used for error checking
-    AVT::VmbAPI::Examples::ApiController apicontrol; 
-    AVT::VmbAPI::Examples::ProgramConfig Config0;
-    AVT::VmbAPI::Examples::ProgramConfig Config1;
-    AVT::VmbAPI::Examples::ProgramConfig Config2;
-   //std::vector<AVT::VmbAPI::Examples::ProgramConfig> figs(Config0,Config1,Config2);
+    AVT::VmbAPI::Examples::ProgramConfig Config;
+    std::string camID;
 
-    AVT::VmbAPI::CameraPtrVector cameras; //initialize the camera list
 
-    std::cout<<"Vimba C++ API Version: "<<apicontrol.GetVersion()<<std::endl; //print the current version
-
-    err = apicontrol.StartUp(); //start up vimba
-    
-    if(VmbErrorSuccess == err)
+    camID = cmdParse(argc, argv);
+    if(camID == "parse_error")
     {
-        if(Config0.getCameraID().empty() &&
-           Config1.getCameraID().empty() &&
-           Config2.getCameraID().empty() ) //if no cams are listed
-        {
-            cameras = apicontrol.GetCameraList(); //get the cams
-            if(cameras.empty()) //if the cameras cannot be found
-            {
-                err = VmbErrorNotFound;
-            }
-        }
-    }
-    else{std::cout<<"Could not start up apicontroller"<<std::endl;}
-    
-    if(err == VmbErrorNotFound)
-    {
-        std::cout<<"No cameras found"<<std::endl;
+        std::cout<<"Program ending, error thrown"<<"\n";
         return 0;
     }
-    else if (err == VmbErrorSuccess)
+    else
     {
-        std::cout<<"Starting the cams"<<std::endl;
-        std::string strCameraID;
+        AVT::VmbAPI::Examples::ApiController control;
+        std::cout<<"Vimba C++ API Version: "<<control.GetVersion()<<std::endl; //print the current version
+        
+        Config.setCameraID(camID);
+        std::cout<<"Setting Camera: "<<camID<<"\n";
 
-        std::vector<bool> truths;
-        if(cameras.size() == CAMERA_COUNT) //make sure that 3 cameras are being used
-        {
-            std::cout<<"Got the right number"<<std::endl;
-            err = cameras[0]->GetID(strCameraID);
-            if(VmbErrorSuccess == err)
-            {
-                Config0.setCameraID(strCameraID);
-                std::cout<<"Got the cam0 set"<<std::endl;
-                truths.push_back(true);
-            }
-            else{std::cout<<"Camera 0 not found. ID number " << cameras[0]->GetID(strCameraID) << std::endl;}
-            err = cameras[1]->GetID(strCameraID);
-            if(VmbErrorSuccess == err)
-            {
-                Config1.setCameraID(strCameraID);
-                std::cout<<"Got the cam1 set"<<std::endl;
-                truths.push_back(true);
-            }
-            else{std::cout<<"Camera 1 not found. ID number " << cameras[1]->GetID(strCameraID) << std::endl;}
-            err = cameras[2]->GetID(strCameraID);
-            if(VmbErrorSuccess == err)
-            {
-                Config2.setCameraID(strCameraID);
-                std::cout<<"Got the cam2 set"<<std::endl;
-                truths.push_back(true);
-            }
-            else{std::cout<<"Camera 2 not found. ID number " << cameras[2]->GetID(strCameraID) << std::endl;}
-        }
-        else {std::cout<<"Wrong number of cameras detected"<<std::endl;}
+        
+        err = control.StartUp(); //start up vimba
 
-        //make sure that all the camera ID's were set
-        if(truths.at(0) == true && truths.at(1) == true && truths.at(2) == true)
-        {
-            err = VmbErrorSuccess;
-            std::cout<<"all cam ID's verified"<<std::endl;
-        }
-        else
-        {
-            err=VmbErrorNotFound;
-            std::cout<<"Not all cam ID's were set"<<std::endl;
-        }
-
-        if(err == VmbErrorSuccess)   //this section should start the image aquisition process
-        {
-            bool state;
-            std::cout<<"entered the thread portion"<<std::endl;
-            state = apicontrol.OpenCameras(cameras); //open all the cameras
-            if(state)
-            {
-                std::cout<<"got into this line"<<std::endl;
-                err = apicontrol.StartContinuousImageAcquisition( Config0 );
-                std::cout<<err<<std::endl;
-
-                if ( VmbErrorSuccess == err )
-                {
-                    std::cout<< "Press <enter> to stop acquisition...\n" ;
-                    getchar();
-
-                    apicontrol.StopContinuousImageAcquisition();
-                }
-                
-                
-            }
-            
-        }
-        std::cout<<"Da shutdown"<<std::endl;
-        apicontrol.ShutDown();
-
-        if ( VmbErrorSuccess == err )
-        {
-            std::cout<<"\nAcquisition stopped.\n" ;
-        }
-        else
-        {
-            std::string strError = apicontrol.ErrorCodeToMessage( err );
-            std::cout<<"\nAn error occurred: " << strError << "\n";
-        }
     }
 
-
-
     
-
     
-
-
-
-
-
+    
+        
     return 0;
 }
 
+std::string cmdParse(int argc, char* argv[]) //function to get the camera ID from the command line
+{
+    std::string item;
+    if(argc < 2){std::cout<<"Error: No arguments provided"<<std::endl;printHelp(argv);return "parse_error";}
+    if(argc > 2){std::cout<<"Error: Too many arguments provided"<<std::endl;printHelp(argv);return "parse_error";}
+    if(argc == 2)
+    {
+        item = argv[1];
+        return item;
+    }
+}
 
-
-// void grabber(AVT::VmbAPI::Examples::ApiController &control,
-//                  AVT::VmbAPI::Examples::ProgramConfig &fig)
-// {
-//     VmbErrorType err;
-//     err = control.StartContinuousImageAcquisition(fig);
-//     if(VmbErrorSuccess == err)
-//     {
-//         std::cout<<"Press to stop"<<std::endl;
-//         getchar();
-
-//         control.StopContinuousImageAcquisition();
-//     }
-// }
-
-
-//  AVT::VmbAPI::Examples::FrameObserver* m_pFrameObserver = new AVT::VmbAPI::Examples::FrameObserver( 
-//             cameras[0], Config0.getFrameInfos(), Config0.getColorProcessing(), Config0.getRGBValue() );
-
-            
-//             err = cameras[0]->StartContinuousImageAcquisition(3,AVT::VmbAPI::IFrameObserverPtr(m_pFrameObserver),
-//             Config0.getAllocAndAnnounce() ? AVT::VmbAPI::FrameAllocation_AllocAndAnnounceFrame : AVT::VmbAPI::FrameAllocation_AnnounceFrame);
-
+void printHelp(char* argv[])
+{
+    std::cout<<"To use the executable " << argv[0] << ", enter the command in like this:"<<std::endl<<std::endl;
+    std::cout<<"\t\t ./FILE_NAME.exe <cam_id>"<<std::endl<<std::endl;
+    std::cout<<"Camera ID's look something like: \'DEV_000F314F3265\'. Only one ID can be accepted as an argument"<<std::endl;
+}
