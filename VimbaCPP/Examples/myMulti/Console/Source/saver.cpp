b95016697ad881d85654e09e3ac1a3f8f7b4c477
Ethan <esmit502@byu.edu>
2022-08-23 15:58:33 -0600

    starting to write a saver function

---
+++ VimbaCPP/Examples/myMulti/Console/Source/saver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+#include "VimbaCPP/Include/VimbaCPP.h"
+#include "Common/StreamSystemInfo.h"
+#include "Common/ErrorCodeToMessage.h"
+#include "Bitmap.h"
+
+void WriteToBitmap(AVT::VmbAPI::FramePtr &pFrame, VmbErrorType &err)
+{
+
+}
+
