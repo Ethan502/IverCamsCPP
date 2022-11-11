990d0b4da7b05d2499c3940114aaaa18c58e7372
Ethan <esmit502@byu.edu>
2022-11-11 12:24:05 -0700

    settings file

--- VimbaPython/Examples/settings_field_adjust.py
+++ VimbaPython/Examples/settings_field_adjust.py
@@ -1,34 +1,52 @@
 from vimba import *
-from statements import *
 import sys
+from typing import Optional
 
 
-print(intro) #Print the first hello
 with Vimba.get_instance() as vimba:
     cams = vimba.get_all_cameras()
-    if len(cams) == 0:
-        print("There are no cameras detected")
+
+    args = sys.argv[1:]
+    argc = len(args)
+
+    isCam = False
+    theCamera = cams[0]
+    settings_file = args[1]
+
+    if(args[0] == "-h" and argc == 1):
+        print("Enter the command as follows: settings_field_adjust.py [cameraID] [fileName]")
+        print("Listing the cameras")
+        for cam in cams:
+            print(f"    {cam.get_id()}")
         sys.exit()
-    counter = 1
-    for cam in cams: #list all the cameras
-        print(f"{counter}. {cam.get_id()}")
-        counter+=1
-    
-    if len(cams) > 1:
-        print(f"Which camera would you like to choose? Enter a number 1-{counter-1}")
-        num = int(input())-1
-        selected_cam = cams[num]
+    elif(argc > 2):
+        print("Incorrect arguments. Abort")
+        sys.exit()
     else:
+        for cam in cams:
+            if(args[0] == cam.get_id()):
+                isCam = True
+                theCamera = cam
+
+
+    if(isCam == False):
+        print("Not a valid camera ID, or the camera is not detected. Abort")
+        sys.exit()
+
+    with cams[0] as cam:
+        cam.load_settings(settings_file, PersistType.All)
+        print("Loaded the settings")
+
+            
+
         
-        selected_cam = cams[0]
-
-    print(f"You selected: {selected_cam.get_id()}")
     
 
 
     
 
 
+    
 
 
 
@@ -42,7 +60,3 @@ with Vimba.get_instance() as vimba:
 
 
 
-        # print(cam.get_id())
-        # exposure_time = cam.ExposureTimeAbs
-        # print(exposure_time.get())
-        # exposure_time.set(12000)
